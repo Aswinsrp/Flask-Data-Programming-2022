df77a2a9636677509612fe79d2f97933f15a81c2
Ashwin Rajapandian <ashwin20.95@gmail.com>
2022-08-06 01:34:53 -0400

    scheduler added

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
+from time import time
 from flask import Flask, Response, Request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from schema.covidData_schema import covidData_serializer, covidDatas_serializer
 import json
+import schedule
+import time
+import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -12,11 +17,11 @@ collection_name = db["Cases"]
 
 @app.route("/", methods=["GET"])
 def home():
-    return "x"
+    return "Welcome"
 
 @app.route("/health", methods=["GET"])
 def getHealth():
-    return "Application running"
+    return "Application started successfully"
 
 @app.route("/getCovidData",methods=["GET"])
 def getCovidData():
@@ -30,6 +35,31 @@ def getCovidData():
         return Response(
             response= json.dumps({"msg":"Failed to retrive Data"}),status=500,mimetype="application/json"
         )
+
+url = "https://covid-19-statistics.p.rapidapi.com/reports"
+headers = {
+    "X-RapidAPI-Key": "ceba13ce3fmsh54baf7333381a96p1e82f6jsnb4f6c7e0220f",
+    "X-RapidAPI-Host": "covid-19-statistics.p.rapidapi.com"
+}
+
+provinces = ["California","New York","Texas","Florida","Illinois"]
+yesterday = datetime.now() - timedelta(1)
+currentDate = str(datetime.strftime(yesterday, '%Y-%m-%d'))
+
+def data_load():
+    for x in provinces:
+        print(x)
+        querystring = {"region_province":x,"iso":"USA","region_name":"US","date":currentDate}
+        r = requests.request("GET", url, headers=headers, params=querystring)
+        if r.status_code == 200:
+            data = r.json()
+            collection_name.insert_one(data)
+
+schedule.every().day.at("01:40").do(data_load)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
 if __name__ == "__main__":
     app.run(debug=True)
